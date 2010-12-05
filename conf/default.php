ca96a8e872d587190c643fc5d1b5580f93ef7515
Elan Ruusamäe <glen@pld-linux.org>
2010-12-05 22:21:49 +0200

    more customization to dformat
    see description at:
    http://getsatisfaction.com/doogie/topics/small_fixes_to_the_plugin

--- conf/default.php
+++ conf/default.php
@@ -3,9 +3,7 @@
 
 # Date format that is used to display the from and to values
 # If you leave this empty '', then the default dformat from /conf/dokuwiki.php will be used.
-$conf['dformat'] = '%d.%m.%Y';
+$conf['dformat'] = '';
 
 # should the end dates for each event be shown?
 $conf['showEndDates'] = 0;
-
-?>
