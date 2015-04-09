6f06864f8f1be2debe4bdc290efd2b44de87cd3e
Tim Ruffing <tim@timruffing.de>
2015-04-09 22:44:23 +0200

    more meaningful PRODID

--- vevent.php
+++ vevent.php
@@ -25,8 +25,7 @@ include_once('functions.php');
 $vevent = urldecode($_GET['vevent']);
 
 $output  = "BEGIN:VCALENDAR\r\n";
-// TODO wrong prodid
-$output .= "PRODID: -//Google Inc//Google Calendar 70.9054//EN" . "\r\n";
+$output .= "PRODID: -//DokuWiki//NONSGML Plugin iCalEvents//EN" . "\r\n";
 $output .= "VERSION:2.0\r\n";
 $output .= $vevent;
 $output .= "END:VCALENDAR\r\n";
